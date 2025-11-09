7b4c2299881fa4b31c72f954f5be19dc003252d4
Gyuri Kim <gyuri981221@gmail.com>
2025-11-09 18:35:26 +0900

    feat: 대학진로 데이터를 위한 랭그래프 구조화

---
+++ backend/LangGraph/graph.py
@@ -0,0 +1,15 @@
+from langgraph.graph import StateGraph, END
+
+from initstate import GraphState
+# from nodes.classify import 
+
+def create_collage_graph_flow():
+    graph = StateGraph(GraphState)
+
+    graph.add_node('classify', )
+    graph.add_node('', )
+    graph.add_node('', )
+    graph.add_node('', )
+
+def create_interview_graph_flow():
+    pass
---
+++ backend/LangGraph/initstate.py
@@ -0,0 +1,10 @@
+from typing import Any, Dict, List, TypedDict
+
+
+
+class GraphState(TypedDict):
+    # 사용자 input
+    question: str
+
+    
+
