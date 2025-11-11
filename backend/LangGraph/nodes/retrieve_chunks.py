07e58b447017e77047bc09e96d8ed5a18b9d6819
Gyuri Kim <gyuri981221@gmail.com>
2025-11-11 09:48:38 +0900

    feat: vectordb에서 유사도가 높은 청크 뽑아오는 노드

---
+++ backend/LangGraph/nodes/retrieve_chunks.py
@@ -0,0 +1,18 @@
+from models import get_embedding_model
+from initstate import GraphState
+from models import get_embedding_model
+from CustomPGvector import CustomPGVector
+from utils import make_conn_str
+
+
+def retrieve_chunks_node(state: GraphState) -> GraphState:
+    """질문과 유사한 chunk 가져오는 노드"""
+    embed = get_embedding_model()
+    vectorstore = CustomPGVector(conn_str=make_conn_str(), embedding_fn=embed)
+    chunk_lst = list()
+    question = state["question"]
+    results = vectorstore.similarity_search_with_score(question, k=5)
+    for doc, score in results:
+        chunk_lst.append({"content": doc.page_content, "score": float(score), "metadata": {**(doc.metadata or {})}})
+    state["chunks"] = chunk_lst
+    return state
