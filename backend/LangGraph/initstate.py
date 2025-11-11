c7130369299ddf2db4464ec3b324f5d6d70190c0
Gyuri Kim <gyuri981221@gmail.com>
2025-11-11 09:47:14 +0900

    refactor: state 추가 및 수정

--- backend/LangGraph/initstate.py
+++ backend/LangGraph/initstate.py
@@ -1,10 +1,28 @@
 from typing import Any, Dict, List, TypedDict
 
+class Chunk(TypedDict):
+    content: str
+    score: float
+    metadata: Dict[str, Any]
 
 
 class GraphState(TypedDict):
-    # 사용자 input
-    question: str
+    user: str # form 기반 input
+    question: str # 사용자 input
+
+    generate_question: str # 질문 재생성
+
+    category: str # classify 이후 카테고리(면접/대학진로)
+
+    chunks: List[Chunk]
+    final_chunks: List[Chunk]
+
+    answer: str
+    final_answer: str
+
+
+    
+    
 
     
 
